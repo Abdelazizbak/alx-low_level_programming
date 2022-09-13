4dd189549e368b17581fb7e9e324cd80dbcd48f8
Abdelazizbak <iscoback8@gmail.com>
2022-09-12 23:13:30 -0700

    adding 4 print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Program to print alphabet letters except q and e followed y new line
+*
+*Return: return 0
+*/
+int letter;
+for (letter = 'a'; letter <= 'z' ; letter++)
+{
+continue;
+}
+letter = tolower(letter);
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
